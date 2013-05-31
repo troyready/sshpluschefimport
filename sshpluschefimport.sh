de140010f53f12f128b6ee167a879f49870ba227
troyready <troy@troyready.com>
2013-05-31 12:11:03 -0700

    removed errant space

--- sshpluschefimport.sh
+++ sshpluschefimport.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cat $HOME/.sshplus_static > $HOME/.sshplus
 echo "# List below is automatically generated from Chef by sshpluschefimport" >> $HOME/.sshplus
-echo "folder: Chef Linux Systems" >> $HOME/.sshplus
+echo "folder:Chef Linux Systems" >> $HOME/.sshplus
 echo " " >> $HOME/.sshplus
 knife search node "os:linux" -i | grep -v -e "items found" -e "^$" | sort | while read servername; do echo "$servername|gnome-terminal|-x ssh $servername" >> $HOME/.sshplus; done
 echo " " >> $HOME/.sshplus
